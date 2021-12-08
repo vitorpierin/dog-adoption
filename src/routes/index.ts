9da45bbcc61ae7ae21e640f6291fd5af2807398a
Vitor Pierin <vitorpierin@hotmail.com>
2021-12-08 12:13:31 +0000

    final project

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,9 +5,9 @@ import * as SearchController from '../controllers/searchController';
 const router = Router();
 
 router.get('/', PageController.home );
-router.get('/dogs', PageController.dog); 
-router.get('/cats', PageController.cat); 
-router.get('/fishes', PageController.fish); 
+router.get('/dog', PageController.dog); 
+router.get('/cat', PageController.cat); 
+router.get('/fish', PageController.fish); 
 
 router.get('/search', SearchController.search)
 
